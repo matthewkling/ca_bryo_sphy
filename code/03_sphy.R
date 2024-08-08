79e8fc65025133df19f7a1d0dfd5a5f3d22cbb83
Matthew Kling <mattkling@gmail.com>
2024-08-08 10:55:24 -0400

    update diversity analyses with sdms

--- code/03_sphy.R
+++ code/03_sphy.R
@@ -1,91 +1,74 @@
 
+# libraries
 library(tidyverse)
 library(spatialphy) # devtools::install_github("matthewkling/spatialphy")
 library(raster)
 library(ape)
 
 
+# load community matrix (from 02_comm.R)
+comm <- readRDS("results/comm/site_by_species.rds")
 
-#### data setup ####
 
-# template spatial grid (15 km res)
-grid <- raster("data/dem15km.tif")
-
-# occurrence data
-occ_data <- read_csv("data/caBryoOccs_2024_07_05-1.csv")
-coordinates(occ_data) <- c("Long", "Lat")
-crs(occ_data) <- "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"
-
-# map occurrences to grid cells
-occ_data <- spTransform(occ_data, crs(grid))
-grid <- extend(grid, occ_data) %>% extend(1)
-grid[] <- 1:ncell(grid)
-occ_data$cell <- raster::extract(grid, occ_data)
-comm <- occ_data %>%
-      as.data.frame() %>%
-      dplyr::select(name, cell) %>%
-      distinct() %>%
-      mutate(pres = 1) %>%
-      spread(name, pres, fill = 0) %>%
-      arrange(cell)
-cell <- comm$cell
-i <- grid[] %in% cell
-comm <- dplyr::select(comm, -cell)
-
-# convert to raster
-comm_ml <- apply(comm, 2, function(x){
-      y <- grid
-      z <- rep(0, ncell(grid))
-      z[i] <- x
-      y[] <- z
-      return(y)}) %>%
-      stack()
-
-# existing reserves
+# load and transform existing protected area data
 reserves <- raster("data/protection_status.tif")
 protected <- reserves %>%
       rasterToPoints() %>%
       as.data.frame()
 coordinates(protected) <- c("x", "y")
 crs(protected) <- crs(reserves)
-protected <- spTransform(protected, crs(comm_ml))
-protected <- rasterize(protected, comm_ml[[1]], field = "protection_status", fun = mean)
+template <- stack("data/cpad_cced_raster_15km.tif")[[2]]
+protected <- spTransform(protected, crs(template))
+protected <- rasterize(protected, template, field = "protection_status", fun = mean)
 protectedNA <- protected
 protected[is.na(protected)] <- 1
 
 
-#### run analyses ####
-
-sphylo <- function(x){
+# function to run spatial phylogenetic analyses for a given phylogeny
+sphylo <- function(tree_file){
       
-      # load tree and align names
-      tree <- read.tree(file = x)[[2]]
-      comm <- subset(comm_ml, intersect(tree$tip.label, names(comm_ml)))
-      tree <- drop.tip(tree, setdiff(tree$tip.label, names(comm)))
-      sp <- sphy(tree, comm)
+      # load tree and intersect taxa with community matrix
+      tree <- read.tree(file = tree_file)[[2]]
+      tree$tip.label <- str_remove_all(tree$tip.label, "-")
+      colnames(comm) <- str_remove_all(colnames(comm), "-")
+      xcom <- comm[, colnames(comm) %in% tree$tip.label]
+      tree <- drop.tip(tree, setdiff(tree$tip.label, colnames(comm)))
+      xcom <- xcom[, tree$tip.label]
+      xcom[is.na(xcom)] <- 0 # NA values not allowed in sphy functions
+      
+      # construct spatial phylo object
+      sp <- sphy(tree, xcom, template)
+      
+      # construct binary version required for canaper randomizations
+      threshold <- .25
+      sp_binary <- sp
+      sp_binary$occ <- apply(sp$occ, 2, function(x) as.integer(x > (threshold * max(x, na.rm = T))))
       
       # run spatial phylogenetic analyses
       div <- sp %>% sphy_diversity()
-      rnd <- sp %>% sphy_rand()
-      reg <- sp %>% sphy_regions(k = 8)
+      rnd <- sp_binary %>% sphy_rand()
+      reg <- sp %>% sphy_regions(k = 5)
       con <- sp %>% sphy_prioritize(protected)
       
+      # combine
       stack(div, rnd, reg, con) %>%
             mask(protectedNA)
 }
 
+# run analyses for mosses and liverworts
 moss <- sphylo("data/mosses_rooted.tree")
 liverwort <- sphylo("data/liverworts_rooted.tree")
 
+# save results
+moss %>% writeRaster("results/sphy/moss_sphy.tif", overwrite = T)
+liverwort %>% writeRaster("results/sphy/liverwort_sphy.tif", overwrite = T)
+names(moss) %>% saveRDS("results/sphy/sphy_layer_names.rds")
 
 
-#### export results and figures ####
-
-# save prioritization rasters
-writeRaster(moss$priority, "results/moss_priority.tif")
-writeRaster(liverwort$priority, "results/liverwort_priority.tif")
+### figures ###
 
 # state bounday data for maps
+select <- dplyr::select
 cali <- map_data("state") %>%
       filter(region == "california")
 cali_pts <- select(cali, long, lat)
